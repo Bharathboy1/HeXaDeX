0dc731a3fb363831046306894ab4e7e1d3f7df36
Bharathboy1 <86122926+Bharathboy1@users.noreply.github.com>
2023-04-23 12:11:41 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -48,8 +48,11 @@ def hpin(client, message):
         if chat_id not in allowed_chat_ids and not all_enabled:
             message.reply_text('Sorry, this is enabled for only admin in this group.')
             return
-   
-    if not message.reply_to_message or message.reply_to_message.from_user.id != 572621020:
+    if not message.reply_to_message:
+        message.reply_text('Please reply to a message from Hexa to pin it.')
+        return
+
+    if not message.reply_to_message.from_user.id != 572621020:
         message.reply_text('Please reply to a message from Hexa to pin it.')
         return   
     
